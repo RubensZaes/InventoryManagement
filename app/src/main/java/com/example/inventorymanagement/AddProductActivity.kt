0f6fa0ec103758015b997e45ebdafaed995bce44
RubensZaes <rubenszaes@gmail.com>
2020-04-12 23:27:50 -0300

    Add Button in  AddProduct

--- app/src/main/java/com/example/inventorymanagement/AddProductActivity.kt
+++ app/src/main/java/com/example/inventorymanagement/AddProductActivity.kt
@@ -2,11 +2,14 @@ package com.example.inventorymanagement
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.add_product.*
 
 class AddProductActivity : AppCompatActivity () {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_product)
+
+        btnTestButton.setOnClickListener { finish() }
     }
 }
