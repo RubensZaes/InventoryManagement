4004e8be0bd9c483ba5f1ca240eb0803963017a6
RubensZaes <rubenszaes@gmail.com>
2020-04-14 13:30:34 -0300

    Add Product Submit

--- app/src/main/java/com/example/inventorymanagement/AddProductActivity.kt
+++ app/src/main/java/com/example/inventorymanagement/AddProductActivity.kt
@@ -1,6 +1,10 @@
 package com.example.inventorymanagement
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.add_product.*
 
@@ -10,6 +14,14 @@ class AddProductActivity : AppCompatActivity () {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_product)
 
-        btnTestButton.setOnClickListener { finish() }
+        val addProductSubmitButton: Button = findViewById(R.id.addProductSubmitButton)
+        addProductSubmitButton.setOnClickListener(View.OnClickListener { btnSubmitButton() })
+    }
+
+    private fun btnSubmitButton() {
+        val database = getSharedPreferences("database", Context.MODE_PRIVATE)
+        database.edit().apply {
+            putString("savedProductName", productNameEditText.text.toString())
+        }.apply()
     }
 }
--- app/src/main/java/com/example/inventorymanagement/MainActivity.kt
+++ app/src/main/java/com/example/inventorymanagement/MainActivity.kt
@@ -3,29 +3,43 @@ package com.example.inventorymanagement
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log.d
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
-        val addInventoryButton: Button = findViewById(R.id.goToAddProductButton)
-        addInventoryButton.setOnClickListener(View.OnClickListener { buttonGoToAddProduct() })
+        val preferences = getSharedPreferences("database", Context.MODE_PRIVATE)
+        val saveNme = preferences.getString("savedProductName", "This value doesn't exist.")
+        d("rubens", "saved message: $saveNme")
+
+        lastSavedProductTextView.text = saveNme
+
+        goToAddProductButton.setOnClickListener {
+            startActivity(Intent(this, AddProductActivity::class.java))
+        }
+
+//        val addInventoryButton: Button = findViewById(R.id.goToAddProductButton)
+//        addInventoryButton.setOnClickListener(View.OnClickListener { buttonGoToAddProduct() })
 
         }
 
-    private fun buttonGoToAddProduct() {
-//        val text = "The button action is working!"
-//        val duration = Toast.LENGTH_SHORT
-//        val toast = Toast.makeText(this, text, duration)
-//        toast.show()
-
-        val intent = Intent(this, AddProductActivity::class.java)
-        startActivity(intent)
-    }
+//    private fun buttonGoToAddProduct() {
+////        val text = "The button action is working!"
+////        val duration = Toast.LENGTH_SHORT
+////        val toast = Toast.makeText(this, text, duration)
+////        toast.show()
+//
+//        val intent = Intent(this, AddProductActivity::class.java)
+//        startActivity(intent)
+//    }
 }
