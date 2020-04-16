49ec261d6439262d6e32f350676d038562e55b73
RubensZaes <rubenszaes@gmail.com>
2020-04-16 11:30:04 -0300

    Add List Products, Total Cost

--- app/src/main/java/com/example/inventorymanagement/MainActivity.kt
+++ app/src/main/java/com/example/inventorymanagement/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.inventorymanagement
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log.d
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -16,5 +17,37 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener {
             startActivity(Intent(this, AddProductActivity::class.java))
         }
+
+        var products = listOf(
+            Product("Kotlin", "Rubens", 2020, 600.00),
+            Product("Android", "Stanley", 2020, 1020.37),
+            Product("Python", "Almeida", 2020, 100.51),
+            Product("Java", "Brandao", 2020, 435.00),
+            Product("Kotlin", "Rubens", 2020, 600.00),
+            Product("Android", "Stanley", 2020, 1020.37),
+            Product("Python", "Almeida", 2020, 100.51),
+            Product("Java", "Brandao", 2020, 435.00),
+            Product("Kotlin", "Rubens", 2020, 600.00),
+            Product("Android", "Stanley", 2020, 1020.37),
+            Product("Python", "Almeida", 2020, 100.51),
+            Product("Java", "Brandao", 2020, 435.00),
+            Product("Kotlin", "Rubens", 2020, 600.00),
+            Product("Android", "Stanley", 2020, 1020.37),
+            Product("Python", "Almeida", 2020, 100.51),
+            Product("Java", "Brandao", 2020, 435.00),
+            Product("Kotlin", "Rubens", 2020, 600.00),
+            Product("Android", "Stanley", 2020, 1020.37),
+            Product("Python", "Almeida", 2020, 100.51),
+            Product("Java", "Brandao", 2020, 435.00)
+        )
+
+        var totalCost = 0.0
+
+        products.forEach {
+            productsTextView.append("${it.name} - ${it.owner} - ${it.yearPurchased} - $${it.cost}\n \n")
+            totalCost += it.cost
+        }
+
+        lastSavedProductTextView.text = "$ $totalCost"
     }
 }
