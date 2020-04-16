e29bc9fe54d0b50763efb8f70f2822da3b66aadc
RubensZaes <rubenszaes@gmail.com>
2020-04-16 15:00:13 -0300

    Creation Class AppConfig

---
+++ app/src/main/java/com/example/inventorymanagement/AppConfig.kt
@@ -0,0 +1,11 @@
+package com.example.inventorymanagement
+
+class AppConfig {
+    companion object{
+        const val filterByName = "Rubens"
+        const val minCost = 40
+        const val maxCost = 1000
+        const val colorTheme = "Dark"
+        const val userEmailAndress = "rubenszaes@gmail.com"
+    }
+}
